6f55bc44596ce55a4543614ed61f9b451168cbf3
Barney <b.wilton1993@gmail.com>
2022-03-02 13:53:43 +0000

    Player can move and rotate now

---
+++ Assets/AnimatorHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BW
+{
+    public class AnimatorHandler : MonoBehaviour
+    {
+        public Animator anim;
+        int vertical;
+        int horizontal;
+    }
+}
